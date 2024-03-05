a73039b213fa9925eb717abeb5eef7a4920318a6
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-03-05 20:43:53 +0200

    finaly?

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import DocumentTitle from '../components/DocumentTitle';
+import css from './PagesStyles.module.css';
 
 export default function HomePage() {
   return (
     <>
       <DocumentTitle>Home</DocumentTitle>
 
-      <div>
-        <h1>Home information</h1>
+      <div className={css.profileBox}>
+        <h1>My profile:</h1>
+        <p>It`s still empty here, but it will be fixed in time 😉</p>
       </div>
     </>
   );
