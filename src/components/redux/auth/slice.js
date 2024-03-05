f188be4f9f3322c633e13bb0d1cff6919a582bb1
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-03-05 12:33:59 +0200

    store and persist

---
+++ src/components/redux/auth/slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: {
+      name: null,
+      email: null,
+    },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+  },
+  extraReducers: () => {},
+});
+
+export const authReducer = authSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import { App } from './components/App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from './components/redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
