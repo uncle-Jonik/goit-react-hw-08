0e4465031f7c0b3648c5a1ef64c4a3209350b75b
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-03-05 13:20:07 +0200

    useAuth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { lazy } from 'react';
 
-const HomePage = lazy(() => import(''));
-const RegisterPage = lazy(() => import(''));
-const LoginPage = lazy(() => import(''));
-const TasksPage = lazy(() => import(''));
+const HomePage = lazy(() => import('../pages/HomePage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LogInPage'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage'));
 
 export const App = () => {
   return <>App components</>;
