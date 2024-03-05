e344b9a840e38188033a7c098c0017111c20c126
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-03-05 16:35:06 +0200

    Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
 import { lazy } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Lauout';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 const HomePage = lazy(() => import('../pages/HomePage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage'));
@@ -6,5 +11,29 @@ const LoginPage = lazy(() => import('../pages/LogInPage'));
 const ContactsPage = lazy(() => import('../pages/ContactsPage'));
 
 export const App = () => {
-  return <>App components</>;
+  const { isRefreshing } = useAuth();
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/register"
+            element={<RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />}
+          />
+          <Route
+            path="/login"
+            element={<RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />}
+          />
+          <Route
+            path="/tasks"
+            element={<PrivateRoute redirectTo="/login" component={<ContactsPage />} />}
+          />
+        </Route>
+      </Routes>
+    </>
+  );
 };
